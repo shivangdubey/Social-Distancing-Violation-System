68ca091c1accf5cd31a5aa5f11cbc5cc8fac28a4
Shivang Dubey <55050487+shivangdubey@users.noreply.github.com>
2023-01-15 12:32:54 +0530

    Add files via upload

---
+++ YOLO_face_people.py
@@ -0,0 +1,108 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
+# Load the YOLO model and names file
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Input video
+cap = cv2.VideoCapture("s2.mp4")
+
+
+# Social distance threshold (in pixels)
+threshold = 200
+
+violation_counter = 0
+def getOutputsNames(net):
+    layersNames = net.getLayerNames()
+    return [layersNames[i - 1] for i in net.getUnconnectedOutLayers()]
+
+while True:
+    _, img = cap.read()
+    height, width, _ = img.shape
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Create a 4D blob from the video
+    blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+
+    # Set the input to the YOLO model
+    net.setInput(blob)
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+
+
+    # Get the output from the YOLO model
+    outs = net.forward(getOutputsNames(net))
+
+    bboxes = []
+    confidences = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                bboxes.append([x, y, w, h])
+                confidences.append(confidence)
+
+
+    # Parse the output and get the bounding boxes
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h, class_id])
+
+    
+
+    # Perform non-maxima suppression to eliminate overlapping bounding boxes
+    indexes = cv2.dnn.NMSBoxes(bboxes, confidences, 0.5, 0.4)
+
+    # Draw the bounding boxes on the image
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h, class_id = boxes[i]
+            label = str(classes[class_id])
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            if label == "person":
+                for (x2, y2, w2, h2, class_id2) in boxes:
+                    if class_id2 == class_id and (x, y, w, h) != (x2, y2, w2, h2):
+                        center_x1 = x + (w/2)
+                        center_y1 = y + (h/2)
+                        center_x2 = x2 + (w2/2)
+                        center_y2 = y2 + (h2/2)
+                        distance = np.sqrt((center_x1 - center_x2)**2 + (center_y1 - center_y2)**2)
+                        if distance < threshold:
+                            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                            cv2.rectangle(img, (x2, y2), (x2 + w2, y2 + h2), (255, 0, 0), 2)
+                            violation_counter += 1
+    cv2.putText(img, "Social Distance Violations: " + str(violation_counter), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+           
